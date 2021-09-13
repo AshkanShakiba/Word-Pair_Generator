9c0454560be373da99c4bf7d7ded81fb1d06273c
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2021-09-13 19:58:55 +0430

    Moving Words class to Words.dart

---
+++ lib/Words.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class Words extends StatefulWidget {
+  @override
+  WordsState createState() {
+    return WordsState();
+  }
+
+}
+
+class WordsState extends State<Words> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text("Word-Pair Generator")
+        ),
+        body: buildList()
+    );
+  }
+
+  final wordPairs=<WordPair>[];
+
+  Widget buildList() {
+    return ListView.builder(
+        padding: EdgeInsets.all(8),
+        itemBuilder: (context, item){
+          if(item.isOdd) {
+            return Divider();
+          }
+          var index=item~/2;
+          if(index>=wordPairs.length){
+            wordPairs.addAll(generateWordPairs().take(11));
+          }
+          return buildRow(wordPairs[index]);
+        }
+    );
+  }
+
+  Widget buildRow(WordPair wordPair){
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+        style: TextStyle(
+            fontSize: 16
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import "package:flutter/material.dart";
-import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'Words.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,54 +13,4 @@ class MyApp extends StatelessWidget{
         theme: ThemeData(primaryColor: Colors.green)
     );
   }
-}
-
-class Words extends StatefulWidget {
-  @override
-  WordsState createState() {
-    return WordsState();
-  }
-
-}
-
-class WordsState extends State<Words> {
-  final wordPairs=<WordPair>[];
-
-  Widget buildList() {
-    return ListView.builder(
-      padding: EdgeInsets.all(8),
-      itemBuilder: (context, item){
-        if(item.isOdd) {
-          return Divider();
-        }
-        var index=item~/2;
-        if(index>=wordPairs.length){
-          wordPairs.addAll(generateWordPairs().take(11));
-        }
-        return buildRow(wordPairs[index]);
-      }
-    );
-  }
-
-  Widget buildRow(WordPair wordPair){
-    return ListTile(
-      title: Text(
-        wordPair.asPascalCase,
-        style: TextStyle(
-          fontSize: 16
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Word-Pair Generator")
-      ),
-      body: buildList()
-    );
-  }
-
 }
